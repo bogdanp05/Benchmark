13a917f2b8608a5064375f8d40f2b8608e974648
Bogdan Petre <bogdan.petre0510@gmail.com>
2019-04-25 11:26:49 +0200

    add raw overhead values to stats

--- viewer/statistics.py
+++ viewer/statistics.py
@@ -12,8 +12,8 @@ def linear_regression(stats, bm):
         y = []
         for idx, dur in enumerate(stats[k]):
             y.append(dur['mean'] - stats[BASE][idx]['mean'])
-        # y = [s['mean'] for s in stats[k]]
         model = LinearRegression().fit(x, y)
         print("Monitoring level %s: b0: %f, b1: %f, score: %f"
               % (k, model.intercept_, model.coef_[0], model.score(x, y)))
+        print("Monitoring level %s overheads(ms): %s" % (k, sorted([round(val*1000, 2) for val in y])))
     print()
