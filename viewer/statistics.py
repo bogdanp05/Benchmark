eb7cb582321d9ce55518c415edc3f58463a6d567
Bogdan Petre <bogdan.petre0510@gmail.com>
2019-04-26 18:58:32 +0200

    mean of the overheads

--- viewer/statistics.py
+++ viewer/statistics.py
@@ -13,7 +13,8 @@ def linear_regression(stats, bm):
         for idx, dur in enumerate(stats[k]):
             y.append(dur['mean'] - stats[BASE][idx]['mean'])
         model = LinearRegression().fit(x, y)
-        print("Monitoring level %s: b0: %f, b1: %f, score: %f"
+        print("Level %s: b0: %f, b1: %f, score: %f"
               % (k, model.intercept_, model.coef_[0], model.score(x, y)))
-        print("Monitoring level %s overheads(ms): %s" % (k, sorted([round(val*1000, 2) for val in y])))
+        print("Level %s overheads(ms): %s, mean: %f" %
+              (k, sorted([round(val*1000, 2) for val in y]), sum(y)/len(y)*1000))
     print()
